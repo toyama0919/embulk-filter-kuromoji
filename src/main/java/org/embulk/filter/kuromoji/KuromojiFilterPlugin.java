b4d2eba9d636d3006a7fb05680608057ba9f95b7
toyama0919 <toyama0919@gmail.com>
2016-06-14 13:59:23 +0900

    refactoring.

--- src/main/java/org/embulk/filter/kuromoji/KuromojiFilterPlugin.java
+++ src/main/java/org/embulk/filter/kuromoji/KuromojiFilterPlugin.java
@@ -1,7 +1,5 @@
 package org.embulk.filter.kuromoji;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -11,26 +9,15 @@ import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
 import org.embulk.spi.Column;
-import org.embulk.spi.Exec;
 import org.embulk.spi.FilterPlugin;
-import org.embulk.spi.Page;
-import org.embulk.spi.PageBuilder;
 import org.embulk.spi.PageOutput;
-import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
 import org.embulk.spi.type.Type;
 import org.embulk.spi.type.Types;
-import org.msgpack.value.Value;
-import org.msgpack.value.ValueFactory;
 
-import com.atilika.kuromoji.ipadic.Token;
-import com.atilika.kuromoji.ipadic.Tokenizer;
-import com.atilika.kuromoji.ipadic.Tokenizer.Builder;
-import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class KuromojiFilterPlugin implements FilterPlugin
@@ -62,6 +49,34 @@ public class KuromojiFilterPlugin implements FilterPlugin
     {
         PluginTask task = config.loadConfig(PluginTask.class);
 
+        Schema outputSchema = buildOutputSchema(task, inputSchema);
+
+        control.run(task.dump(), outputSchema);
+    }
+
+    @Override
+    public PageOutput open(TaskSource taskSource, final Schema inputSchema, final Schema outputSchema, final PageOutput output)
+    {
+        return new KuromojiPageOutput(taskSource, inputSchema, outputSchema, output);
+    }
+
+    /**
+     * @param inputSchema
+     * @param task
+     * @return
+     */
+    private Schema buildOutputSchema(PluginTask task, Schema inputSchema) {
+        ImmutableList.Builder<Column> builder = buildOutputColumns(task, inputSchema);
+
+        return new Schema(builder.build());
+    }
+
+    /**
+     * @param task
+     * @param inputSchema
+     * @return
+     */
+    private ImmutableList.Builder<Column> buildOutputColumns(PluginTask task, Schema inputSchema) {
         ImmutableList.Builder<Column> builder = ImmutableList.builder();
         Map<String, Column> map = Maps.newHashMap();
         int i = 0;
@@ -85,120 +100,6 @@ public class KuromojiFilterPlugin implements FilterPlugin
             final Column column = e.getValue();
             builder.add(new Column(i++, column.getName(), column.getType()));
         }
-
-        Schema outputSchema = new Schema(builder.build());
-        control.run(task.dump(), outputSchema);
-    }
-
-    @Override
-    public PageOutput open(TaskSource taskSource, final Schema inputSchema, final Schema outputSchema, final PageOutput output)
-    {
-        final PluginTask task = taskSource.loadTask(PluginTask.class);
-
-        Builder builder = new Tokenizer.Builder();
-        if (task.getDictionaryPath().isPresent()) {
-            try {
-                builder.userDictionary(task.getDictionaryPath().get());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        final Tokenizer tokenizer = builder.build();
-        final List<Column> keyNameColumns = Lists.newArrayList();
-
-        for (String keyName : task.getKeyNames()) {
-            keyNameColumns.add(outputSchema.lookupColumn(keyName));
-        }
-
-        return new PageOutput() {
-            private PageReader reader = new PageReader(inputSchema);
-            private PageBuilder builder = new PageBuilder(Exec.getBufferAllocator(), outputSchema, output);
-
-            @Override
-            public void finish() {
-                builder.finish();
-            }
-
-            @Override
-            public void close() {
-                builder.close();
-            }
-
-            @Override
-            public void add(Page page) {
-                reader.setPage(page);
-                while (reader.nextRecord()) {
-                    setValue(builder);
-                    builder.addRecord();
-                }
-            }
-
-            /**
-             * @param builder
-             */
-            private void setValue(PageBuilder builder) {
-                if (task.getKeepInput()) {
-                    for (Column inputColumn : inputSchema.getColumns()) {
-                        if (reader.isNull(inputColumn)) {
-                            builder.setNull(inputColumn);
-                            continue;
-                        }
-                        if (Types.STRING.equals(inputColumn.getType())) {
-                            builder.setString(inputColumn, reader.getString(inputColumn));
-                        } else if (Types.BOOLEAN.equals(inputColumn.getType())) {
-                            builder.setBoolean(inputColumn, reader.getBoolean(inputColumn));
-                        } else if (Types.DOUBLE.equals(inputColumn.getType())) {
-                            builder.setDouble(inputColumn, reader.getDouble(inputColumn));
-                        } else if (Types.LONG.equals(inputColumn.getType())) {
-                            builder.setLong(inputColumn, reader.getLong(inputColumn));
-                        } else if (Types.TIMESTAMP.equals(inputColumn.getType())) {
-                            builder.setTimestamp(inputColumn, reader.getTimestamp(inputColumn));
-                        } else if (Types.JSON.equals(inputColumn.getType())) {
-                            builder.setJson(inputColumn, reader.getJson(inputColumn));
-                        }
-                    }
-                }
-
-                for (Column column : keyNameColumns) {
-                    List<Token> tokens = tokenizer.tokenize(reader.getString(column));
-                    for (Map<String, String> setting : task.getSettings()) {
-                        String suffix = setting.get("suffix");
-                        String method = setting.get("method");
-                        Column outputColumn = outputSchema.lookupColumn(column.getName() + MoreObjects.firstNonNull(suffix, ""));
-                        List<Value> outputs = Lists.newArrayList();
-                        for (Token token : tokens) {
-                            if (!isOkPartsOfSpeech(token)) { continue; }
-                            String word = null;
-                            if ("base_form".equals(method)) {
-                                word = MoreObjects.firstNonNull(token.getBaseForm(), token.getSurface());
-                            } else if ("reading".equals(method)) {
-                                word = MoreObjects.firstNonNull(token.getReading(), token.getSurface());
-                            } else if ("surface_form".equals(method)) {
-                                word = token.getSurface();
-                            }
-                            outputs.add(ValueFactory.newString(word));
-                        }
-                        if (outputColumn.getType().equals(Types.STRING)) {
-                            Joiner joiner = Joiner.on(MoreObjects.firstNonNull(setting.get("delimiter"), ",")).skipNulls();
-                            builder.setString(outputColumn, joiner.join(outputs));
-                        } else if (outputColumn.getType().equals(Types.JSON)) {
-                            builder.setJson(outputColumn, ValueFactory.newArray(outputs));
-                        }
-                    }
-                }
-            }
-
-            private boolean isOkPartsOfSpeech(Token token) {
-                if (!task.getOkPartsOfSpeech().isPresent()) { return true; };
-                for (String okPartsOfSpeech : task.getOkPartsOfSpeech().get()) {
-                    if (token.getAllFeaturesArray()[0].equals(okPartsOfSpeech)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        };
+        return builder;
     }
 }
---
+++ src/main/java/org/embulk/filter/kuromoji/KuromojiPageOutput.java
@@ -0,0 +1,146 @@
+package org.embulk.filter.kuromoji;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.embulk.config.TaskSource;
+import org.embulk.filter.kuromoji.KuromojiFilterPlugin.PluginTask;
+import org.embulk.spi.Column;
+import org.embulk.spi.Exec;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageBuilder;
+import org.embulk.spi.PageOutput;
+import org.embulk.spi.PageReader;
+import org.embulk.spi.Schema;
+import org.embulk.spi.type.Types;
+import org.msgpack.value.Value;
+import org.msgpack.value.ValueFactory;
+
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
+import com.atilika.kuromoji.ipadic.Tokenizer.Builder;
+import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Lists;
+
+public class KuromojiPageOutput implements PageOutput
+{
+    private final PluginTask task;
+    private final Tokenizer tokenizer;
+    private final List<Column> keyNameColumns;
+    private final PageReader reader;
+    private final PageBuilder builder;
+    private final Schema inputSchema;
+    private final Schema outputSchema;
+
+    public KuromojiPageOutput(TaskSource taskSource, Schema inputSchema, Schema outputSchema, PageOutput output) {
+        this.task = taskSource.loadTask(PluginTask.class);
+        this.inputSchema = inputSchema;
+        this.outputSchema = outputSchema;
+
+        Builder builder = new Tokenizer.Builder();
+        if (task.getDictionaryPath().isPresent()) {
+            try {
+                builder.userDictionary(task.getDictionaryPath().get());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.tokenizer = builder.build();
+        this.keyNameColumns = Lists.newArrayList();
+
+        for (String keyName : task.getKeyNames()) {
+            this.keyNameColumns.add(outputSchema.lookupColumn(keyName));
+        }
+        this.reader = new PageReader(inputSchema);
+        this.builder = new PageBuilder(Exec.getBufferAllocator(), outputSchema, output);
+    }
+
+    @Override
+    public void finish() {
+        builder.finish();
+    }
+
+    @Override
+    public void close() {
+        builder.close();
+    }
+
+    @Override
+    public void add(Page page) {
+        reader.setPage(page);
+        while (reader.nextRecord()) {
+            setValue(builder);
+            builder.addRecord();
+        }
+    }
+
+    /**
+     * @param builder
+     */
+    private void setValue(PageBuilder builder) {
+        if (task.getKeepInput()) {
+            for (Column inputColumn : inputSchema.getColumns()) {
+                if (reader.isNull(inputColumn)) {
+                    builder.setNull(inputColumn);
+                    continue;
+                }
+                if (Types.STRING.equals(inputColumn.getType())) {
+                    builder.setString(inputColumn, reader.getString(inputColumn));
+                } else if (Types.BOOLEAN.equals(inputColumn.getType())) {
+                    builder.setBoolean(inputColumn, reader.getBoolean(inputColumn));
+                } else if (Types.DOUBLE.equals(inputColumn.getType())) {
+                    builder.setDouble(inputColumn, reader.getDouble(inputColumn));
+                } else if (Types.LONG.equals(inputColumn.getType())) {
+                    builder.setLong(inputColumn, reader.getLong(inputColumn));
+                } else if (Types.TIMESTAMP.equals(inputColumn.getType())) {
+                    builder.setTimestamp(inputColumn, reader.getTimestamp(inputColumn));
+                } else if (Types.JSON.equals(inputColumn.getType())) {
+                    builder.setJson(inputColumn, reader.getJson(inputColumn));
+                }
+            }
+        }
+
+        for (Column column : keyNameColumns) {
+            List<Token> tokens = tokenizer.tokenize(reader.getString(column));
+            for (Map<String, String> setting : task.getSettings()) {
+                String suffix = setting.get("suffix");
+                String method = setting.get("method");
+                Column outputColumn = outputSchema.lookupColumn(column.getName() + MoreObjects.firstNonNull(suffix, ""));
+                List<Value> outputs = Lists.newArrayList();
+                for (Token token : tokens) {
+                    if (!isOkPartsOfSpeech(token)) { continue; }
+                    String word = null;
+                    if ("base_form".equals(method)) {
+                        word = MoreObjects.firstNonNull(token.getBaseForm(), token.getSurface());
+                    } else if ("reading".equals(method)) {
+                        word = MoreObjects.firstNonNull(token.getReading(), token.getSurface());
+                    } else if ("surface_form".equals(method)) {
+                        word = token.getSurface();
+                    }
+                    outputs.add(ValueFactory.newString(word));
+                }
+                if (outputColumn.getType().equals(Types.STRING)) {
+                    Joiner joiner = Joiner.on(MoreObjects.firstNonNull(setting.get("delimiter"), ",")).skipNulls();
+                    builder.setString(outputColumn, joiner.join(outputs));
+                } else if (outputColumn.getType().equals(Types.JSON)) {
+                    builder.setJson(outputColumn, ValueFactory.newArray(outputs));
+                }
+            }
+        }
+    }
+
+    private boolean isOkPartsOfSpeech(Token token) {
+        if (!task.getOkPartsOfSpeech().isPresent()) { return true; };
+        for (String okPartsOfSpeech : task.getOkPartsOfSpeech().get()) {
+            if (token.getAllFeaturesArray()[0].equals(okPartsOfSpeech)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
